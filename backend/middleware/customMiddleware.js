b419c32d1f20ea4b263e0343ba3c9be9cd2996b0
faripanah <farnaz.jamilpanah@metropolia.fi>
2026-02-28 07:27:03 +0200

    backup before restructure

--- backend/middleware/customMiddleware.js
+++ backend/middleware/customMiddleware.js
@@ -2,7 +2,7 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" });
   };
   
-  const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, request, response, next) => {
     console.error(error.message);
   
     response.status(500);
@@ -11,7 +11,7 @@ const unknownEndpoint = (request, response) => {
     });
   };
   
-  const requestLogger = (request, response, next) => {
+const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
     console.log("Path:  ", request.path);
     console.log("Body:  ", request.body);
